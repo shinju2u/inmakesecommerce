75423c701eed5bca3b7afed814e4def6c2cbd056
shinju <shinju2u@gmail.com>
2023-03-12 01:30:23 +0530

    uploaded the first time

---
+++ ecommerceproject/shop/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+from ecommerceproject import settings
+app_name='shop'
+
+urlpatterns = [
+
+  path('',views.allProdCat,name='allProdCat'),
+path('<slug:c_slug>/',views.allProdCat,name='products_by_category'),
+path('<slug:c_slug>/<slug:product_slug>/',views.proDetail,name='prodCatdetail')
+
+]
